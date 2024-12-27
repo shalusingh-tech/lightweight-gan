11737909e26df0aaeb4c9087f50ca03d0f0a94e2
ParadoxOfCode <68162265+shalusingh-tech@users.noreply.github.com>
2024-12-28 00:54:59 +0530

    Update lightweight_gan.py

--- lightweight_gan/lightweight_gan.py
+++ lightweight_gan/lightweight_gan.py
@@ -35,7 +35,7 @@ from adabelief_pytorch import AdaBelief
 
 # asserts
 
-#assert torch.cuda.is_available(), 'You need to have an Nvidia GPU with CUDA installed.'
+device = "GPU" if torch.cuda.is_available() else "CPU"  # 'You need to have an Nvidia GPU with CUDA installed.'
 
 # constants
 
